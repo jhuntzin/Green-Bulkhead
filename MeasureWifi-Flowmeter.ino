c72c5f180431f466357d3dbfdddb6ca16c806e92
jhuntzin <36557854+jhuntzin@users.noreply.github.com>
2018-10-11 15:25:24 -0400

    Add files via upload
    Updated Functional Data Logging

---
+++ MeasureWifi-Flowmeter.ino
@@ -0,0 +1,199 @@
+/*
+ ESP8266 --> ThingSpeak Channel via MKR1000 Wi-Fi
+ 
+ This sketch sends the Wi-Fi Signal Strength (RSSI) of an ESP8266 to a ThingSpeak
+ channel using the ThingSpeak API (https://www.mathworks.com/help/thingspeak).
+ 
+ Requirements:
+ 
+   * ESP8266 Wi-Fi Device
+   * Arduino 1.6.9+ IDE
+   * Additional Boards URL: http://arduino.esp8266.com/stable/package_esp8266com_index.json
+   * Library: esp8266 by ESP8266 Community
+ 
+ ThingSpeak Setup:
+ 
+   * Sign Up for New User Account - https://thingspeak.com/users/sign_up
+   * Create a new Channel by selecting Channels, My Channels, and then New Channel
+   * Enable one field
+   * Note the Channel ID and Write API Key
+    
+ Setup Wi-Fi:
+  * Enter SSID 
+  * Enter Password
+  
+ Tutorial: http://nothans.com/measure-wi-fi-signal-levels-with-the-esp8266-and-thingspeak
+   
+ Created: Feb 1, 2017 by Hans Scharler (http://nothans.com)
+*/
+
+/*
+Liquid flow rate sensor -DIYhacking.com Arvind Sanjeev
+
+Measure the liquid/water flow rate using this code. 
+Connect Vcc and Gnd of sensor to arduino, and the 
+signal line to arduino digital pin 2.
+ 
+ */
+
+#include <ESP8266WiFi.h>
+
+// Wi-Fi Settings
+const char* ssid = "GamingHouse"; // your wireless network name (SSID)
+const char* password = "patisgay69"; // your Wi-Fi network password
+
+WiFiClient client;
+
+//Flowmeter Settings
+byte statusLed    = 13;
+
+byte sensorInterrupt = 0;  // 0 = digital pin 2
+byte sensorPin       = 2;
+
+// The hall-effect flow sensor outputs approximately 4.5 pulses per second per
+// litre/minute of flow.
+float calibrationFactor = 4.5;
+
+volatile byte pulseCount;  
+
+float flowRate;
+unsigned int flowMilliLitres;
+unsigned long totalMilliLitres;
+
+unsigned long oldTime;
+
+
+
+// ThingSpeak Settings
+const int channelID = 537825;
+String writeAPIKey = "BJJUPXRKO9XADTV9"; // write API key for your ThingSpeak Channel
+const char* server = "api.thingspeak.com";
+const int postingInterval = 5 * 1000; // post data every 20 seconds
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    
+  // Set up the status LED line as an output
+  pinMode(statusLed, OUTPUT);
+  digitalWrite(statusLed, HIGH);  // We have an active-low LED attached
+  
+  pinMode(sensorPin, INPUT);
+  digitalWrite(sensorPin, HIGH);
+
+  pulseCount        = 0;
+  flowRate          = 0.0;
+  flowMilliLitres   = 0;
+  totalMilliLitres  = 0;
+  oldTime           = 0;
+
+
+  // The Hall-effect sensor is connected to pin 2 which uses interrupt 0.
+  // Configured to trigger on a FALLING state change (transition from HIGH
+  // state to LOW state)
+  attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+
+
+  }
+}
+
+void loop() {
+  if (client.connect(server, 80)) {
+    
+    // Measure Signal Strength (RSSI) of Wi-Fi connection
+    long rssi = WiFi.RSSI();
+
+    // Measure Flowmeter
+    if((millis() - oldTime) > 1000)    // Only process counters once per second
+  { 
+    // Disable the interrupt while calculating flow rate and sending the value to
+    // the host
+    detachInterrupt(sensorInterrupt);
+        
+    // Because this loop may not complete in exactly 1 second intervals we calculate
+    // the number of milliseconds that have passed since the last execution and use
+    // that to scale the output. We also apply the calibrationFactor to scale the output
+    // based on the number of pulses per second per units of measure (litres/minute in
+    // this case) coming from the sensor.
+    flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
+    
+    // Note the time this processing pass was executed. Note that because we've
+    // disabled interrupts the millis() function won't actually be incrementing right
+    // at this point, but it will still return the value it was set to just before
+    // interrupts went away.
+    oldTime = millis();
+    
+    // Divide the flow rate in litres/minute by 60 to determine how many litres have
+    // passed through the sensor in this 1 second interval, then multiply by 1000 to
+    // convert to millilitres.
+    flowMilliLitres = (flowRate / 60) * 1000;
+    
+    // Add the millilitres passed in this second to the cumulative total
+    totalMilliLitres += flowMilliLitres;
+      
+    unsigned int frac;
+    
+    // Print the flow rate for this second in litres / minute
+    //Serial.print("Flow rate: ");
+    //Serial.print(int(flowRate));  // Print the integer part of the variable
+    //Serial.print(".");             // Print the decimal point
+    // Determine the fractional part. The 10 multiplier gives us 1 decimal place.
+    frac = (flowRate - int(flowRate)) * 10;
+    //Serial.print(frac, DEC) ;      // Print the fractional part of the variable
+    //Serial.print("L/min");
+    // Print the number of litres flowed in this second
+    //Serial.print("  Current Liquid Flowing: ");             // Output separator
+    //Serial.print(flowMilliLitres);
+    //Serial.print("mL/Sec");
+
+    // Print the cumulative total of litres flowed since starting
+    //Serial.print("  Output Liquid Quantity: ");             // Output separator
+    //Serial.println("mL");
+
+     
+
+    // Reset the pulse counter so we can start incrementing again
+    pulseCount = 0;
+    
+    // Enable the interrupt again now that we've finished sending output
+    attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+    }
+    
+    String totalLiters = String(totalMilliLitres/1000);
+    
+    // Construct API request body
+    String body = "field1=";
+           body += String(rssi);
+           body += "&field2=";
+           body += totalLiters;
+           
+    
+    //Serial.print("RSSI: ");
+    //Serial.println(rssi); 
+    
+
+    client.println("POST /update HTTP/1.1");
+    client.println("Host: api.thingspeak.com");
+    client.println("User-Agent: ESP8266 (nothans)/1.0");
+    client.println("Connection: close");
+    client.println("X-THINGSPEAKAPIKEY: " + writeAPIKey);
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.println("Content-Length: " + String(body.length()));
+    client.println("");
+    client.print(body);
+    Serial.println(body);
+
+  }
+  client.stop();
+
+  // wait and then post again
+  delay(postingInterval);
+}
+void pulseCounter()
+{
+  // Increment the pulse counter
+  pulseCount++;
+}
---
+++ WifIDataLogging-Node2-SoilMoisture.ino
@@ -0,0 +1,116 @@
+/*
+ ESP8266 --> ThingSpeak Channel via MKR1000 Wi-Fi
+ 
+ This sketch sends the Wi-Fi Signal Strength (RSSI) of an ESP8266 to a ThingSpeak
+ channel using the ThingSpeak API (https://www.mathworks.com/help/thingspeak).
+ 
+ Requirements:
+ 
+   * ESP8266 Wi-Fi Device
+   * Arduino 1.6.9+ IDE
+   * Additional Boards URL: http://arduino.esp8266.com/stable/package_esp8266com_index.json
+   * Library: esp8266 by ESP8266 Community
+ 
+ ThingSpeak Setup:
+ 
+   * Sign Up for New User Account - https://thingspeak.com/users/sign_up
+   * Create a new Channel by selecting Channels, My Channels, and then New Channel
+   * Enable one field
+   * Note the Channel ID and Write API Key
+    
+ Setup Wi-Fi:
+  * Enter SSID
+  * Enter Password
+  
+ Tutorial: http://nothans.com/measure-wi-fi-signal-levels-with-the-esp8266-and-thingspeak
+   
+ Created: Feb 1, 2017 by Hans Scharler (http://nothans.com)
+
+
+
+ Adapted by John Huntzinger August 16, 2018
+ University of Maryland, College Park
+ ENST Undergraduate
+*/
+
+#include <ESP8266WiFi.h>
+//#include <dht.h>
+// Wi-Fi Settings
+const char* ssid = "GreenBulkhead"; // your wireless network name (SSID)
+const char* password = "melodicviolet513"; // your Wi-Fi network password
+
+WiFiClient client;
+
+// ThingSpeak Settings
+const int channelID = 537825;
+String writeAPIKey = "BJJUPXRKO9XADTV9"; // write API key for your ThingSpeak Channel
+const char* server = "api.thingspeak.com";
+const int postingInterval = 5 * 1000; // post data every 5 seconds
+
+//dht DHT;
+
+
+int moisturePin = A0;
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode (D0, INPUT); //NodeMCU pin mapping: Digital 0 = D0 etc
+  
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+}
+
+void loop() {
+  if (client.connect(server, 80)) {
+    
+    // Measure Signal Strength (RSSI) of Wi-Fi connection
+    long rssi = WiFi.RSSI();
+
+    //Measure Other Things
+    int sensorValue = analogRead(moisturePin);
+    int inverseSensorVal = map(sensorValue, 0, 1024, 1024, 0);
+    
+    //Map 1-1024 Sensor Values to a 0-100 scale of wetness
+    //int percentValue = map(sensorValue, 1024, 550, 0, 100);  //550 = full submersion **NEEDS TO BE CALIBRATED**
+
+    Serial.print("Moisture Value: ");
+    Serial.println(sensorValue);
+    
+    
+    // Construct API request body
+    
+    //Field 3
+    
+    String body = "field3=";
+           body += String(rssi);
+           
+    //Serial.print("RSSI: ");
+    //Serial.println(rssi);
+
+    //Field 4
+
+           body += "&field4=";
+           body += String(inverseSensorVal);
+           
+    Serial.println(body);
+    Serial.println("");
+    
+    client.println("POST /update HTTP/1.1");
+    client.println("Host: api.thingspeak.com");
+    client.println("User-Agent: ESP8266 (nothans)/1.0");
+    client.println("Connection: close");
+    client.println("X-THINGSPEAKAPIKEY: " + writeAPIKey);
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.println("Content-Length: " + String(body.length()));
+    client.println("");
+    client.print(body);
+    
+  }
+  client.stop();
+
+  // wait and then post again
+  delay(postingInterval);
+}
